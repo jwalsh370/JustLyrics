2217bc37dcd70e753e7252d5a43936d4a907cb53
jwalsh370 <jwalsh370@gmail.com>
2017-05-28 18:18:10 -0700

    add custom font

--- app/src/main/java/com/jahanwalsh/justlyrics/MainActivity.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/MainActivity.java
@@ -1,13 +1,69 @@
 package com.jahanwalsh.justlyrics;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.graphics.Typeface;
 
-public class MainActivity extends AppCompatActivity {
+//public class MainActivity extends AppCompatActivity {
+//    private Button mFindRestaurantsButton;
+//    private EditText mLocationEditText;
+//    private TextView mAppNameTextView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+//        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+//        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+//        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+//        mAppNameTextView.setTypeface(ostrichFont);
+//
+//        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String location = mLocationEditText.getText().toString();
+//                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+//                intent.putExtra("location", location);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//}
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.button) Button mButton;
+//    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.justLyricsTextView) TextView mjustLyricsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface streets = Typeface.createFromAsset(getAssets(), "fonts/streets.ttf");
+        mjustLyricsTextView.setTypeface(streets);
+
+        mButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if (v == mButton) {
+//            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, LyricsActivity.class);
+//            intent.putExtra("location", location);
+            startActivity(intent);
+        }
     }
 }
