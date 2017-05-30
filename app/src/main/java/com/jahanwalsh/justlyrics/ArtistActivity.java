d5a4accc9ecbebf8c2bb3cf02d6ff164e3cb1d7c
jwalsh370 <jwalsh370@gmail.com>
2017-05-30 16:00:57 -0700

    fix button revealing array information

--- app/src/main/java/com/jahanwalsh/justlyrics/ArtistActivity.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/ArtistActivity.java
@@ -3,9 +3,9 @@ package com.jahanwalsh.justlyrics;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,12 +16,12 @@ import butterknife.ButterKnife;
 
 
 public class ArtistActivity extends AppCompatActivity {
-    @Bind(R.id.artistTextView)
-    public TextView mArtistTextView;
+    @Bind(R.id.songTextView)
+    public TextView mSongTextView;
     @Bind(R.id.listView)
     ListView mListView;
 
-    public static final String TAG = ArtistActivity.class.getSimpleName();
+//    public static final String TAG = ArtistActivity.class.getSimpleName();
 
     private String[] artists = new String[] {"MF Doom", "Cricket", "Prince", "Tupac"};
     private String[]  songs = new String[] {"Beer", "Breakfast", "Raspberry Beret", "California Love"};
@@ -29,30 +29,31 @@ public class ArtistActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_artist);
         ButterKnife.bind(this);
 
         mListView = (ListView) findViewById(R.id.listView);
-        mArtistTextView = (TextView) findViewById(R.id.artistTextView);
+        mSongTextView = (TextView) findViewById(R.id.songTextView);
 
-        MyArtistArrayAdapter adapter = new MyArtistArrayAdapter(this, android.R.layout.simple_list_item_1,
-                artists, songs); //must match constructor!
+//        MyArtistArrayAdapter adapter = new MyArtistArrayAdapter(this, android.R.layout.simple_list_item_1,
+//                artists, songs); //must match constructor!
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists);
 
         mListView.setAdapter(adapter);
+
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String artists = ((TextView)view).getText().toString();
                 Toast.makeText(ArtistActivity.this, artists, Toast.LENGTH_LONG).show();
-                Log.v("TAG", "In the onItemClickListener!");
             }
         });
 
         Intent intent = getIntent();
-        String artist = intent.getStringExtra("artist");
-        mArtistTextView.setText("Here are all the songs from: " + artist);
-        Log.d("TAG", "In the onCreate method!");
+        String song = intent.getStringExtra("song");
+        mSongTextView.setText("This artist has: " + song);
 
     }
 }
--- app/src/main/java/com/jahanwalsh/justlyrics/MainActivity.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/MainActivity.java
@@ -1,18 +1,14 @@
 package com.jahanwalsh.justlyrics;
 
 
-import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.graphics.Typeface;
-
-
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
