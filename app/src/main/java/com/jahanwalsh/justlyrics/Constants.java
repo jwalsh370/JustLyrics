4f4ac2b4747c7ebcbe2f46f61699eb4b0d54f999
jwalsh370 <jwalsh370@gmail.com>
2017-05-31 14:04:35 -0700

    add constants, api key info to properties and build files, add okhttp to compiler

---
+++ app/src/main/java/com/jahanwalsh/justlyrics/Constants.java
@@ -0,0 +1,10 @@
+package com.jahanwalsh.justlyrics;
+
+/**
+ * Created by jahanwalsh on 5/31/17.
+ */
+
+public class Constants {
+    public static final String API_CONSUMER_KEY = BuildConfig.API_CONSUMER_KEY;
+
+}
