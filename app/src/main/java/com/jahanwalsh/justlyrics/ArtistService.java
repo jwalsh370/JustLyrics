b512c1e0d3af61b6c164500dbe86f1984c6401c8
jwalsh370 <jwalsh370@gmail.com>
2017-06-01 09:16:56 -0700

    add artistservice api key request and response parameters

--- app/src/main/java/com/jahanwalsh/justlyrics/ArtistService.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/ArtistService.java
@@ -1,27 +1,46 @@
 package com.jahanwalsh.justlyrics;
 
-import javax.security.auth.callback.Callback;
+import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import okhttp3.Callback;
+
+import okhttp3.Call;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
-import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
-import se.akerfeldt.okhttp.signpost.SigningInterceptor;
-
+import okhttp3.Request;
+import okhttp3.Response;
 /**
  * Created by jahanwalsh on 5/31/17.
  */
 
 public class ArtistService {
+
+    public static final String TAG = ArtistService.class.getSimpleName();
+
     public static void findArtist(String artist, Callback callback) {
-        OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(Constants.API_CONSUMER_KEY);
 
         OkHttpClient client = new OkHttpClient.Builder()
-                .addInterceptor(new SigningInterceptor(consumer))
                 .build();
 
         HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.API_BASE_URL).newBuilder();
-        urlBuilder.addQueryParameter(Constants.API_ARTIST_QUERY_PARAMETER, artist);
+        urlBuilder.addQueryParameter(Constants. API_ARTIST_QUERY_PARAMETER,artist);
+        urlBuilder.addQueryParameter(Constants.API_KEY_QUERY_PARAMETER, Constants.API_CONSUMER_KEY);
         String url = urlBuilder.build().toString();
 
+        Log.d("url", url);
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
     }
 }
+
--- app/src/main/java/com/jahanwalsh/justlyrics/Constants.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/Constants.java
@@ -8,5 +8,8 @@ public class Constants {
     public static final String API_CONSUMER_KEY = BuildConfig.API_CONSUMER_KEY;
     public static final String API_BASE_URL = "https://api.musixmatch.com/ws/1.1/artist.search?format=jsonp&callback=callback";
     public static final String API_ARTIST_QUERY_PARAMETER = "artist";
+    public static final String API_KEY_QUERY_PARAMETER = "appid";
+
 
 }
+
