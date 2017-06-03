134d0c0ff3bec7ecd0dbff2a6fc718a249be6059
jwalsh370 <jwalsh370@gmail.com>
2017-06-02 18:07:19 -0700

    add  parceler and page adapters

--- app/src/main/java/com/jahanwalsh/justlyrics/models/Artist.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/models/Artist.java
@@ -6,12 +6,11 @@ import java.util.ArrayList;
 @Parcel
 public class Artist {
     private String mName;
-    private String mTrack;
+
 
     public Artist() {}
 
     public Artist (String name){
-
         this.mName = name;
 //        this.mTrack = track;
     }
