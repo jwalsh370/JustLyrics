9180101b2064e9fb57e57dfb88f4b769f15f8748
jwalsh370 <jwalsh370@gmail.com>
2017-06-20 07:52:48 -0700

    add one more animation.

--- app/src/main/java/com/jahanwalsh/justlyrics/adapters/FirebaseArtistViewHolder.java
+++ app/src/main/java/com/jahanwalsh/justlyrics/adapters/FirebaseArtistViewHolder.java
@@ -4,36 +4,21 @@ package com.jahanwalsh.justlyrics.adapters;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.jahanwalsh.justlyrics.Constants;
+
 import com.jahanwalsh.justlyrics.R;
 import com.jahanwalsh.justlyrics.models.Artist;
-import com.jahanwalsh.justlyrics.ui.ArtistDetailActivity;
-import com.jahanwalsh.justlyrics.ui.MainActivity;
-import com.jahanwalsh.justlyrics.util.ItemTouchHelperAdapter;
 import com.jahanwalsh.justlyrics.util.ItemTouchHelperViewHolder;
-import com.squareup.picasso.Picasso;
-
-import org.parceler.Parcels;
-
-import java.util.ArrayList;
 
 public class FirebaseArtistViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
 
 
-    public ImageView mAlbumImageView;
+    public ImageView mLyricTextView;
 
 
         View mView;
@@ -47,7 +32,7 @@ public class FirebaseArtistViewHolder extends RecyclerView.ViewHolder implements
 
         public void bindArtist(Artist artist) {
             TextView lyricTextView = (TextView) mView.findViewById(R.id.lyricTextView);
-            ImageView albumImageView = (ImageView) mView.findViewById(R.id.albumImageView);
+            ImageView dragIcon = (ImageView) mView.findViewById(R.id.dragIcon);
 
             lyricTextView.setText(artist.getLyric());
 
---
+++ app/src/main/java/com/jahanwalsh/justlyrics/util/OnArtistSelectedListener.java
@@ -0,0 +1,14 @@
+package com.jahanwalsh.justlyrics.util;
+
+import com.jahanwalsh.justlyrics.models.Artist;
+
+import java.util.ArrayList;
+
+/**
+ * Created by jahanwalsh on 6/19/17.
+ */
+
+    public interface OnArtistSelectedListener {
+
+    }
+
